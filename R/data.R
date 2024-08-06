fb601a575b12282f1bc075843da84027db8bd99c
emilio-berti <emilio.berti90@gmail.com>
2024-08-06 10:09:02 +0200

    file_folder: add amphibio

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A subset of data from TetraEU, including only adults individuals.
 #' For details, see <https://doi.org/10.1111/geb.13138>.
 #'
-#' @format ## `who`
+#' @format ## `tetraeu`
 #' A data frame with 60,453 rows and 2 columns:
 #' \describe{
 #'   \item{prey}{Prey name}
@@ -17,7 +17,7 @@
 #' A subset of data from EltonTraits, including only species with known body mass.
 #' For details, see <https://doi.org/10.1890/13-1917.1>.
 #'
-#' @format ## `who`
+#' @format ## `mammals`
 #' A data frame with 4,041 rows and 2 columns:
 #' \describe{
 #'   \item{species}{Species name}
@@ -31,7 +31,7 @@
 #' A subset of data from EltonTraits, including only species with known body mass.
 #' For details, see <https://doi.org/10.1890/13-1917.1>.
 #'
-#' @format ## `who`
+#' @format ## `birds`
 #' A data frame with 9,123 rows and 2 columns:
 #' \describe{
 #'   \item{species}{Species name}
@@ -40,12 +40,28 @@
 #' @source <https://doi.org/10.5061/dryad.jm63xsj7b>
 "birds"
 
+#' Body Mass for Amphibians.
+#'
+#' A subset of data from AmphiBIO, including only species with known body mass
+#' or known body length, from which body masses was imputed.
+#' For details, see <https://doi.org/10.1038/sdata.2017.123>.
+#'
+#' @format ## `amphibians`
+#' A data frame with 5,227 rows and 2 columns:
+#' \describe{
+#'   \item{species}{Species name}
+#'   \item{mass}{Body mass (g)}
+#'   \item{imputed}{Was body mass imputed using major axis regression?}
+#' }
+#' @source <https://doi.org/10.5061/dryad.jm63xsj7b>
+"amphibians"
+
 #' GBIF Taxonomic Backbone
 #'
 #' The GBIF taxonomic backbone for the species in the datasets obtained 
 #' running the function `gbif_taxonomy`.
 #'
-#' @format ## `who`
+#' @format ## `backbone`
 #' A data frame with 13,158 rows and 2 columns:
 #' \describe{
 #'   \item{species}{Species name}
