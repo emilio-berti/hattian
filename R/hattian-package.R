f1ea1f709779480e6a577f9eff2f4d9a4b5977fa
emilio-berti <emilio.berti90@gmail.com>
2024-08-12 11:49:41 +0200

    :wrench: go back to @docType (deprecated)

--- R/hattian-package.R
+++ R/hattian-package.R
@@ -2,7 +2,7 @@
 #'
 #' @description A DESCRIPTION OF THE PACKAGE
 #'
-#' @docType _PACKAGE
+#' @docType package
 #' @name hattian-package
 #' @aliases hattian
 #' @useDynLib hattian, .registration = TRUE
